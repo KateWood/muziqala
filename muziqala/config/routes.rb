389a0ceb4b16325c1afcdd5b97ce3f8e7d7614af
Kate Wood <katewood611@gmail.com>
2015-06-17 16:46:12 -0700

    adds sessions controller, routes, helper, and login view

--- muziqala/config/routes.rb
+++ muziqala/config/routes.rb
@@ -3,12 +3,16 @@ Rails.application.routes.draw do
   get "users" => "users#index"
   get "users/new" => "users#new"
   post "users" => "users#create"
+  
+  get "login" => "sessions#new"
+  post "login" => "sessions#create"
+  delete "logout" => "sessions#destroy"
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#new'
+  root "sessions#new"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
