3094691327cc2408e47bd22fa33f4f30117280cf
Kate Wood <katewood611@gmail.com>
2015-06-18 10:27:32 -0700

    adds FactoryGirl gem

--- muziqala/app/models/user.rb
+++ muziqala/app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_secure_password
+	validates :email, presence: true
 
 	def full_name
 		"#{first_name} #{last_name}"
---
+++ muziqala/spec/models/user_spec.rb
@@ -0,0 +1,6 @@
+require "rails_helper"
+
+RSpec.describe User do
+	it "is invalid without an email address" do
+	end 
+end
