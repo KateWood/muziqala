dc4f4a94aa1088e04565071c04ec4824a5e7735c
Kate Wood <katewood611@gmail.com>
2015-06-19 10:42:11 -0700

    adds comments to users controller

--- muziqala/app/controllers/users_controller.rb
+++ muziqala/app/controllers/users_controller.rb
@@ -1,12 +1,18 @@
 class UsersController < ApplicationController
+	# Sets user for show, edit, update, and destroy using user_params
+	before_action :set_user, only: [:show, :edit, :update, :destroy]
+	
+	# Shows all users
 	def index
 		@users = User.all
 	end
 
+	# Sets user on sign up page
 	def new
 		@user = User.new
 	end
 
+	# Creates a user upon sign up
 	def create
 		@user = User.new(user_params)
 		if @user.save
@@ -17,6 +23,7 @@ class UsersController < ApplicationController
 	end
 
 private
+	# Defines user params for use in controller actions
 	def user_params
 		params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
 	end
