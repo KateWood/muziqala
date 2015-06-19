cd0c32ec1ca84fd4be6178125991caef6b39a323
Kate Wood <katewood611@gmail.com>
2015-06-19 09:44:48 -0700

    separates auth create and bcrypt create controller actions

--- muziqala/app/controllers/sessions_controller.rb
+++ muziqala/app/controllers/sessions_controller.rb
@@ -2,17 +2,20 @@ class SessionsController < ApplicationController
 	def new
 	end
 
-	def create
+	def create_auth
 		@auth = request.env['omniauth.auth']
 		session['auth'] = @auth
 		redirect_to users_path
-		# user = User.where(email: params[:login][:email]).first
-		# if user && user.authenticate(params[:login][:password])
-		# 	session[:user_id] = user.id.to_s
-		# 	redirect_to users_path
-		# else
-		# 	render :new
-		# end
+	end
+
+	def create
+		user = User.where(email: params[:login][:email]).first
+		if user && user.authenticate(params[:login][:password])
+			session[:user_id] = user.id.to_s
+			redirect_to users_path
+		else
+			render :new
+		end
 	end
 
 	def destroy
