e0161a0a85ab9d0771e9df086a76a507733004c5
Kate Wood <katewood611@gmail.com>
2015-06-19 10:40:05 -0700

    adds comments to sessions controller

--- muziqala/app/controllers/sessions_controller.rb
+++ muziqala/app/controllers/sessions_controller.rb
@@ -1,13 +1,16 @@
 class SessionsController < ApplicationController
+	# Login for a new session
 	def new
 	end
 
+	# Creates a session for a user logged in via Facebook
 	def create_auth
 		@auth = request.env['omniauth.auth']
 		session['auth'] = @auth
 		redirect_to users_path
 	end
 
+	# Creates a session for a user logged in manually (bcrypt)
 	def create
 		user = User.where(email: params[:login][:email]).first
 		if user && user.authenticate(params[:login][:password])
@@ -18,6 +21,7 @@ class SessionsController < ApplicationController
 		end
 	end
 
+	# Ends user session at logout
 	def destroy
 		session.delete(:user_id)
 		redirect_to login_path
