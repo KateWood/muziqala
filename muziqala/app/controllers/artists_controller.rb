294a1d96934739b68741f9fb9a5a690d27d3a655
Your Name <carlos.mez1@gmail.com>
2015-06-18 17:51:19 -0700

    adds search field to search through artists, albums, and song title. Needs to fix show page for albums and song title.

--- muziqala/app/controllers/artists_controller.rb
+++ muziqala/app/controllers/artists_controller.rb
@@ -1,7 +1,20 @@
 class ArtistsController < ApplicationController
   def index
+
+    if params[:album]
+      @artists = RSpotify::Album.search(params[:artist_name])
+    elsif  params[:artist]
+      @artists = RSpotify::Artist.search(params[:artist_name])
+    elsif  params[:track]
+      @artists = RSpotify::Track.search(params[:artist_name])
+    # if !params[:artist_name].empty?
+    #  @artists = RSpotify::Artist.search(params[:artist_name])
+   else
+     redirect_to root_path
+   end
   end
 
   def show
+    @artist = RSpotify::Artist.find(params[:id])
   end
 end
