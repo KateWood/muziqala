50df54d30a8fcf740e9790a6849e807893ca358b
Kate Wood <katewood611@gmail.com>
2015-06-20 17:47:07 -0700

    added development code for spotify oauth

--- muziqala/config/initializers/omniauth.rb
+++ muziqala/config/initializers/omniauth.rb
@@ -1,6 +1,7 @@
 require 'rspotify/oauth'
 
 Rails.application.config.middleware.use OmniAuth::Builder do
+	provider :developer unless Rails.env.production?
 	provider :spotify, ENV["spotify_client_id"], ENV["spotify_secret"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
 end
 
