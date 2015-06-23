e71d5c93778de5a8a432792874c44f9071fc19a0
Kate Wood <katewood611@gmail.com>
2015-06-23 10:29:17 -0700

    adds capybara feature spec for album search

--- muziqala/spec/features/artists_spec.rb
+++ muziqala/spec/features/artists_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 feature "Search Spotify" do
-	scenario "returns search results" do
+	scenario "returns search results for artist" do
       visit root_path
       fill_in "search_keywords", with: "queen"
       choose 'search_artist'
       click_button "Search"
       expect(page).to have_content("Artists Matching queen")
+    end
+
+    scenario "returns search results for album" do
+      visit root_path
+      fill_in "search_keywords", with: "queen"
+      choose 'search_album'
+      click_button "Search"
+      expect(page).to have_content("Albums Matching queen")
     end
 end
