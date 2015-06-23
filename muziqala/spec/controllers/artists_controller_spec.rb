062e4b3a958087e47660f60f156b21dafe386bee
Kate Wood <katewood611@gmail.com>
2015-06-23 09:50:24 -0700

    adds working show test to artists_controller_spec.rb

--- muziqala/spec/controllers/artists_controller_spec.rb
+++ muziqala/spec/controllers/artists_controller_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe ArtistsController, type: :controller do
 
   describe "GET #show" do
     it "returns http success" do
-      get :show
+      get :show, id: "1dfeR4HaWDbWqFHLkxsg1d"
       expect(response).to have_http_status(:success)
     end
   end
