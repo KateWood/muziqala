68bea3ae3a36abe9bc6cc3fd0834da8c3e411608
Ariel Kanamori <ari.kanamori@gmail.com>
2015-06-23 18:54:09 -0700

    generated Playlist model and UserPlaylist model

---
+++ spec/factories/playlists.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :playlist do
+    name "MyString"
+  end
+
+end
---
+++ spec/factories/user_playlists.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :user_playlist do
+    user nil
+playlist nil
+  end
+
+end
