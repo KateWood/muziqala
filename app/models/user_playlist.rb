e4ad006d0290f8b4c2ea77e23fe770aa775d70de
Kate Wood <katewood611@gmail.com>
2015-06-24 14:50:01 -0700

    removes old models user and user_playlist

--- app/models/user_playlist.rb
+++
@@ -1,4 +0,0 @@
-class UserPlaylist < ActiveRecord::Base
-  belongs_to :user
-  belongs_to :playlist
-end
--- spec/factories/user_playlists.rb
+++
@@ -1,7 +0,0 @@
-FactoryGirl.define do
-  factory :user_playlist do
-    user nil
-playlist nil
-  end
-
-end
