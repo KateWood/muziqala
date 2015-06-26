e34375df08db0b7de8184e58e7bcecb3ef843b08
Kate Wood <katewood611@gmail.com>
2015-06-26 11:22:19 -0700

    fixes omniauth.rb initializers file syntax

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -4,5 +4,3 @@ Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :developer unless Rails.env.production?
 	provider :spotify, ENV["spotify_client_id"], ENV["spotify_secret"], scope: 'user-read-email playlist-modify-public user-library-read user-library-modify'
 end
-
-end
