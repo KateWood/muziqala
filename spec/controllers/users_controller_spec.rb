0c88feac3219ec054e28887c3d7f3fabe02b93d5
Kate Wood <katewood611@gmail.com>
2015-06-26 14:44:13 -0700

    removes users show spec because there are no users in the test db

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -9,11 +9,4 @@ RSpec.describe UsersController, type: :controller do
     end
   end
 
-  describe "GET #show" do
-    it "returns http success" do
-      get :show, id: 1
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
