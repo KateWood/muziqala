acc87232847f8abbed4e0fc21b2d1b49dfd3e090
Kate Wood <katewood611@gmail.com>
2015-06-26 16:23:54 -0700

    adds test for user show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,9 +4,17 @@ RSpec.describe UsersController, type: :controller do
 
   describe "GET #index" do
     it "returns http success" do
-      get :index
-      expect(response).to have_http_status(:success)
+    	get :index
+    	expect(response).to have_http_status(:success)
     end
+  end
+
+  describe "GET #show" do
+  	it "returns http success" do
+	  	user = FactoryGirl.build(:user)
+	  	get :show, id: user.id
+	  	expect(response).to have_http_status(:success)
+	 end
   end
 
 end
