79765951e1aa7812effbd0a74a1b17758423d84d
Kate Wood <katewood611@gmail.com>
2015-06-27 13:26:47 -0700

    redirects to user show after login - allows signup without image

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,8 @@ class SessionsController < ApplicationController
 
 	# Defines user for show page
 	def show
-		redirect_to root_path unless session['auth']
-		@auth = session['auth']
+		redirect_to root_path unless session[:user_id]
+		@auth = session[:user_id]
 	end
 
 	# Creates a session for a user logged in via Facebook
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
 		if User.where(:spotify_id => @auth["info"]["id"]).first
 			@user = User.where(:spotify_id => @auth["info"]["id"]).first
 		else
-			@user = User.create(
+			if @auth["info"]["images"][0]["url"]
+				@user = User.create(
 				:display_name => @auth["info"]["display_name"],
 				:token => @auth["credentials"]["token"],
 				:refresh_token => @auth["credentials"]["refresh_token"],
@@ -20,9 +21,19 @@ class SessionsController < ApplicationController
 				:spotify_id => @auth["info"]["id"],
 				:user_uri => @auth["info"]["uri"]
 				)
+			else
+			@user = User.create(
+				:display_name => @auth["info"]["display_name"],
+				:token => @auth["credentials"]["token"],
+				:refresh_token => @auth["credentials"]["refresh_token"],
+				:image_url => 'muziqala_logo.jpq',
+				:spotify_id => @auth["info"]["id"],
+				:user_uri => @auth["info"]["uri"]
+				)
+			end
 		end
 		session[:user_id] = @user.id
-		redirect_to sessions_show_path
+		redirect_to user_path(@user)
 	end
 
 	# Ends auth session at logout
