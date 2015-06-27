e996259fb5108deab470530cd099239f17851755
Kate Wood <katewood611@gmail.com>
2015-06-27 14:24:52 -0700

    changes method to save track - still doesn't work

--- app/controllers/tracks_controller.rb
+++ app/controllers/tracks_controller.rb
@@ -7,14 +7,9 @@ class TracksController < ApplicationController
     	@track = Track.new
     end
 
-    def create
-        @track = RSpotify::Track.find_by(params[:id])
-        if Track.where(:spotify_id => @track["id"]).first
-            redirect_to artist_show_path
-        else
-            @track = Track.create(:spotify_id => @track["id"])
-            redirect_to artist_show_path
-        end
+    def create id
+        @track = Track.create(:spotify_id => id)
+        redirect_to artist_show_path
     end
 
 end
