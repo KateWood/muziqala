53d0c958ef7e631f532a03e3a6db5f7baa8f53ff
Kate Wood <katewood611@gmail.com>
2015-06-28 11:09:03 -0700

    adds track_info method to tracks model

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,4 +2,8 @@ class Track < ActiveRecord::Base
   belongs_to :album
   has_many :playlisttracks
   has_many :playlists, through: :playlisttracks
+
+  def track_info
+  	RSpotify::Track.find(:spotify_id)
+  end
 end
