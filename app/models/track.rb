f0538afb2cde0882c72102f19dcba70b57d7acf8
Kate Wood <katewood611@gmail.com>
2015-06-28 12:15:44 -0700

    removes track_info method from track model

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,8 +2,4 @@ class Track < ActiveRecord::Base
   belongs_to :album
   has_many :playlisttracks
   has_many :playlists, through: :playlisttracks
-
-  def track_info
-  	RSpotify::Track.find(:spotify_id)
-  end
 end
