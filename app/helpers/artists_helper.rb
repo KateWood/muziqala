0d6c2a74afa01eb9c48eaffc428ce39f36af9ac8
Your Name <carlos.mez1@gmail.com>
2015-06-28 18:13:27 -0700

    initial setup for converting time from ms to min/sec in helper method

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -1,10 +1,10 @@
 module ArtistsHelper
-  def format_time(track.duraction_ms)
+  def format_time(track.duration_ms)
   minutes = track.duration_ms / 1000
 
   seconds = track.duration_ms / 60
 
-  remainder_seconds = 
+  # remainder_seconds = 
 
 
 end
